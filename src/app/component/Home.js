c9160c3a7268eab5e81710e854460ea12b5df1d3
vergelbarit35@gmail.com <vergelbarit35@gmail.com>
2019-03-11 12:36:57 +0800

    event example

--- src/app/component/Home.js
+++ src/app/component/Home.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 export default class Home extends Component {
+    //props
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+    onMakeOlder() {
+        this.age+=3;
+        console.log(this.age);
+    }
+
     render() {
         // console.log(this.props);
         // console.log(this.props.user.nickName);
@@ -10,7 +20,7 @@ export default class Home extends Component {
                 <p>In a new Component!</p>
                 <p>Hello passed name: {this.props.name}</p>
                 <p>Nickname is: {this.props ? this.props.user.nickName : ''}</p>
-                <p>Hello passed age: {this.props.age}</p>
+                <p>Hello passed age: {this.age}</p>
                 <br></br>
                 <p>Hobbies are</p>
                 <ul>
@@ -19,6 +29,9 @@ export default class Home extends Component {
                 <p>children data:
                     {this.props.children}
                 </p>
+                <button onClick={()=> this.onMakeOlder()}>Make me older</button>
+                {/* Other technique */}
+                {/* <button onClick={this.onMakeOlder.bind(this)}className="btn btn-success">Make me older</button> */}
             </div>
         }
         return (
