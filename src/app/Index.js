a3f8b17affcf9f15322c41cb9422221f056f8765
vergelbarit35@gmail.com <vergelbarit35@gmail.com>
2019-03-11 10:35:05 +0800

    topic2

--- src/app/Index.js
+++ src/app/Index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-// import { render } from "react-dom";
+import Header from './component/Header';
+import Home from './component/Home';
 
 class AppIndex extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello!</h1>
+            <div className="container">
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    <Header/>
+                </div>
             </div>
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    <Home/>
+                </div>
+            </div>
+        </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import AppIndex from './app/Index';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
